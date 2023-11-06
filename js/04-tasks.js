7fddb85c185bbf4fafb64e2f6f2a3ec7313fcb07
Alexey Simak <40754302+simakol@users.noreply.github.com>
2023-11-06 19:13:33 +0200

    lesson-06

--- js/04-tasks.js
+++ js/04-tasks.js
@@ -35,15 +35,6 @@ printInfo(
  * та хвилин HH:MM.
  */
 
-const hours = Math.floor(totalMinutes / 60);
-const minutes = totalMinutes % 60;
-console.log(hours);
-console.log(minutes);
-
-const doubleDigitHours = String(hours).padStart(2, 0);
-const doubleDigitMinutes = String(minutes).padStart(2, 0);
-console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
-
 function formatTime(minutes) {}
 
 console.log(formatTime(70)); // "01:10"
