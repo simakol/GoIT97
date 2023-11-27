e1ebf0f4230f96284defed15b46e9887ce950c9d
Alexey Simak <40754302+simakol@users.noreply.github.com>
2023-11-27 22:14:40 +0200

    lesson-11

--- js/03-methods.js
+++ js/03-methods.js
@@ -6,84 +6,120 @@
  * - Метод bind
  */
 
-const showThis = function (a, b, arr) {
-  console.log(a, b, arr);
-  console.log("showThis -> this", this);
-};
+// Ці методи потрібні нам для того щоб мати змогу змінити контекст виконання функції(наприклад, під час передачі фукнції як колбека). call/apply - викликають фукнцію на місці, відрізняються варіантами передачі аргументів. bind - не викликає функцію, а лише повертає її копію з новим this
 
-showThis();
+// const showThis = function (a, b, arr) {
+//   console.log(a, b, arr);
+//   console.log("showThis -> this", this);
+// };
 
-const objA = {
-  a: 5,
-  b: 10,
-};
+// showThis();
 
-showThis.call(objA, 5, 1, [100, 200, 300]);
-showThis.apply(objA, [5, 1, [100, 200, 300]]);
+// const objA = {
+//   a: 5,
+//   b: 10
+// };
 
-const objB = {
-  x: 788,
-  y: 25,
-};
+// func.call(newThis, ...args)
+// // func.apply(newThis, [...args])
+// showThis.call(objA, 5, 1, [100, 200, 300]);
+// showThis.apply(objA, [5, 1, [100, 200, 300]]);
 
-showThis.call(objB, 1, 1, 2);
-showThis.apply(objB, [1, 1, 2]);
+// const objB = {
+//   x: 788,
+//   y: 25,
+// };
 
-showThis();
+// showThis.call(objB, 1, 1, 2);
+// showThis.apply(objB, [1, 1, 2]);
 
-/**
- * -------------------------------
- */
-const changeColor = function (color) {
-  console.log("changeColor -> this", this);
-  this.color = color;
-};
-
-const hat = {
-  color: "black",
-};
-
-changeColor.call(hat, "orange");
-console.log(hat);
-
-const sweater = {
-  color: "green",
-};
-
-changeColor.call(sweater, "blue");
-console.log(sweater);
+// showThis();
 
 /**
  * -------------------------------
  */
-const changeHatColor = changeColor.bind(hat);
-const changeSweaterColor = changeColor.bind(sweater);
 
-changeHatColor("yellow");
-console.log(hat);
+// const changeColor = function (color) {
+//   console.log("changeColor -> this", this);
+//   this.color = color;
+// };
 
-changeSweaterColor("red");
-console.log(sweater);
+// const hat = {
+//   color: "black",
+// };
+
+// changeColor.call(hat, "orange");
+// console.log(hat);
+
+// const sweater = {
+//   color: "green",
+// };
+
+// changeColor.call(sweater, "blue");
+// console.log(sweater);
 
 /**
  * -------------------------------
  */
-const counter = {
-  value: 0,
-  increment(value) {
-    console.log("increment -> this", this);
-    this.value += value;
-  },
-  decrement(value) {
-    console.log("decrement -> this", this);
-    this.value -= value;
-  },
-};
 
-const updateCounter = function (value, operation) {
-  operation(value);
-};
+//* func.bind(newThis, ...args) - поверне копію функції з новим this
 
-updateCounter(10, counter.increment.bind(counter));
-updateCounter(5, counter.decrement.bind(counter));
-console.log(counter);
+// const jacob = {
+//   tag: "Jacob",
+//   showTag() {
+//     // метод, який являється колбек фукнцією (тому що нижче ми його передамо як аргумент)
+//     console.log("showTag -> this", this);
+//     console.log("showTag -> this.tag", this.tag);
+//   },
+// };
+
+// // invokeAction - функція вищого порядку
+// function invokeAction(action) {
+//   console.log(action);
+//   action(); // викликаємо колбек фукнцію showTag без контексту, тому що він втратився під час передачі цієї фукнції як колбеку, відповідно this = undefined
+// }
+
+// invokeAction(jacob.showTag.bind(jacob));
+
+// const changeHatColor = changeColor.bind(hat);
+// const changeSweaterColor = changeColor.bind(sweater);
+
+// changeHatColor("yellow");
+// console.log(hat);
+
+// changeSweaterColor("red");
+// console.log(sweater);
+
+/**
+ * -------------------------------
+ */
+
+// const counter = {
+//   value: 0,
+//   increment(value) {
+//     console.log("increment -> this", this);
+//     this.value += value;
+//   },
+//   decrement(value) {
+//     console.log("decrement -> this", this);
+//     this.value -= value;
+//   },
+// };
+
+// const updateCounter = function (value, operation) {
+//   operation(value);
+// };
+
+// updateCounter(10, counter.increment.bind(counter));
+// updateCounter(5, counter.decrement.bind(counter));
+// console.log(counter);
+
+// const incrementValue = counter.increment.bind(counter);
+// const decrementValue = counter.decrement.bind(counter);
+
+// incrementValue(150)
+// decrementValue(48)
+// decrementValue(42)
+
+
+// console.log(counter);
--- js/04-prototype.js
+++ js/04-prototype.js
@@ -7,48 +7,58 @@
  * - Ланцюжки прототипів
  */
 
-const animal = {
-  legs: 4,
-};
+// const animal = {
+//   legs: 4,
+// };
 
-const dog = Object.create(animal);
-dog.name = "Mango";
+// const dog = Object.create(animal); // створює новий пустий обʼєкт і вказує обʼєкт animal прототипом (тобто, нам будуть доступні всі властивості і методи обʼєкту animal)
+// dog.name = "Oskar";
 
-console.log(dog);
-console.log(dog.name);
-console.log(dog.legs);
-console.log(dog.hasOwnProperty("name"));
-console.log(dog.hasOwnProperty("legs"));
+// console.log(dog);
+// console.log(dog.name);
+// console.log(dog.legs);
+// console.log(dog.hasOwnProperty("name"));
+// console.log(dog.hasOwnProperty("legs"));
+// console.log(Object.hasOwn(dog, "name"));
+// console.log(Object.hasOwn(dog, "legs"));
 
-for (const key in dog) {
-  if (dog.hasOwnProperty(key)) {
-    console.log(key); // "name"
-  }
-}
+// console.log("name" in dog);
+// console.log("legs" in dog); // оператор in не бачить різниці між тим чи є властивість власна, чи прототипна, тому коли ми перебираємо обʼєкти за допомогою циклу for in, завжди треба робити перевірку на те чи є ця властивіть власна
+
+// for (const key in dog) {
+//   if (Object.hasOwn(dog, key)) {
+//     console.log(dog[key]);
+//   }
+// }
 
 /**
  * ----------------------------------------
  */
-const objC = { c: "objC prop" };
 
-const objB = Object.create(objC);
-objB.b = "objB prop";
+// const objC = { c: "objC prop" };
 
-const objA = Object.create(objB);
-objA.a = "objA prop";
+// const objB = Object.create(objC);
+// objB.b = "objB prop";
 
-console.log(objA);
-console.log(objB);
-console.log(objC);
+// const objA = Object.create(objB);
+// objA.a = "objA prop";
 
-console.log(objA.hasOwnProperty("a"));
-console.log(objA.a);
+// console.log(objA);
+// console.log(objB);
+// console.log(objC);
 
-console.log(objA.hasOwnProperty("b"));
-console.log(objA.b);
+// console.log(objA.hasOwnProperty("a")); //
+// console.log(Object.hasOwn(objA, "a")); // true
+// console.log(objA.a); //
 
-console.log(objA.hasOwnProperty("c"));
-console.log(objA.c);
+// console.log(objA.hasOwnProperty("b"));
+// console.log(Object.hasOwn(objA, "b")); // false
+// console.log(objA.b);
 
-console.log(objA.hasOwnProperty("x"));
-console.log(objA.x);
+// console.log(objA.hasOwnProperty("c"));
+// console.log(Object.hasOwn(objA, "c")); // false
+// console.log(objA.c);
+
+// console.log(objA.hasOwnProperty("x"));
+// console.log(Object.hasOwn(objA, "x")); // false
+// console.log(objA.x); // undefined
